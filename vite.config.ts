ba2b9bb564783a5f2436ea06579ad4ae42224868
Yelyzaveta-star <bethkorobka@gmail.com>
2025-12-07 17:54:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADD THIS LINE ⬇⬇⬇⬇⬇
+  base: "/solow-nextgen-spark/",
+
   server: {
     host: "::",
     port: 8080,
